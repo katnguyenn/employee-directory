943a98d78938e3c09f8f13c1407617dbab069ca9
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-04 14:44:47 -0800

    header title and style

---
+++ employeedirectory/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Header.css'
+
+function Header() {
+    return (
+            <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Use the search box to narrow results or carrots to filter by heading</p>
+            </div>
+        
+    );
+}
+
+export default Header;
