b6aa8b1b40b285ace7e8a79edbd6e55505d32111
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-09 16:27:40 -0800

    display people from the U.S.

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default {
     getUsers: function() {
-        return axios.get("https://randomuser.me/api/?page=4&results=100")
+        return axios.get("https://randomuser.me/api/?results=100&nat=us")
     }
 };
