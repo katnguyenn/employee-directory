1b9d7c5c9c72de5fbe782cc330a855a57b29b0e7
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-12 12:11:32 -0800

    mapped over headings to display each name

---
+++ src/components/DataHeader.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+
+
+const DataHeader = (props) => {
+    const headings = [
+        { name: "Image" },
+        { name: "Name" },
+        { name: 'Phone' },
+        { name: 'Email' },
+        { name: "DOB" }
+    ]
+
+    return (
+
+
+        <thead>
+            <tr>
+                {headings.map(({ name }) => {
+                    return (
+                        <th
+                            className="header-col"
+                            key={name}
+                            onClick={() => {
+                                props.setSortCol(name)
+                            }}
+
+                        >
+                            {name}
+
+                        </th>
+                    )
+
+                })}
+
+
+            </tr>
+
+        </thead>
+
+
+
+
+
+    )
+}
+
+export default DataHeader;
