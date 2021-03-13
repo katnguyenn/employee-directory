6ed063405c31d0526a867f26f6d8a2de9c2b17ba
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-12 20:39:51 -0800

    changed paragraph to add filter by name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
             <div className="header">
             <h1>Employee Directory</h1>
-            <p>Use the search box to narrow results or carrots to filter by heading</p>
+            <p>Use the search box to narrow results or filter by name</p>
             </div>
         
     );
